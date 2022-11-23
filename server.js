b451485371fafcf23cb714333846f948c727ab6e
Uroš Barjaktarević <ubarjaktarev@gmail.com>
2022-11-23 18:21:52 +0100

    start the project, create title page, start work on home page and navbar

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/captains-blog')
+    .then(console.log('Successfully connected to the database.'))
+    .catch(err => console.log(err));
+
+app.set('views', 'views')
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000!')
+})
+
+app.get('/', (req, res) => {
+    res.render('title')
+})
+
+app.get('/home', async (req, res) => {
+    res.render('home')
+})
+
